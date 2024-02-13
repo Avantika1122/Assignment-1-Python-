756c7876020371d73e1d2d526c6fd582de14c923
Avantika1122 <139642385+Avantika1122@users.noreply.github.com>
2024-02-13 23:54:59 +0530

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,7 @@
+# write a python program to findout the given number is even or odd number
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
