24a5ac19f1f90c3abdeb921dd28a2df329be5428
Avantika1122 <139642385+Avantika1122@users.noreply.github.com>
2024-02-13 23:54:34 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,8 @@
+# Write a python program to find biggest of two numbers
+
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+if a>b:
+    print(f'{a} is a maximum number')
+else:
+    print(f'{b} is a maximum number')
